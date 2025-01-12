debe1f662050d6cf6ad65ebdc6b0cf07e46d0389
Nilso J. <nilsoemiro@gmail.com>
2025-01-12 08:32:54 +0000

    changes

---
+++ powershell-scripts/s3.ps1
@@ -0,0 +1,8 @@
+Import-Module AWS.Tools.S3
+
+$region = "us-east-1"
+
+$bucketName = Read-Host -Prompt 'Enter the S3 Bucket name'
+
+Write-Host "AWS Region: $region"
+Write-Host "S3 Bcuket: $bucketname"
